15ad9a1e9e13564a9597c81845ede81a4abebe00
Logan M Roe <Lmproe27@gmail.com>
2023-08-30 09:15:13 -0500

    streamlit push

--- src/pages/2 Monster Stats.py
+++ src/pages/2 Monster Stats.py
@@ -15,13 +15,29 @@ df_histo = df[['size','alignment','xp','natural_ac','challenge_rating','strength
 
 
 df_histo.columns= df_histo.columns.str.title().str.strip().str.replace('_',' ')
-col1,col2 = st.columns(2)
 
-histo = col1.selectbox('Histogram data select', placeholder="Aboleth", options=sorted(df_histo.columns))
+histo = st.selectbox('Histogram data select', placeholder="Natural Ac", options=sorted(df_histo.columns))
 if histo:
     try:
         st.plotly_chart(px.histogram(df_histo,histo))
     except BaseException:
         st.error(f'''
                  {histo.title()} could not be plotted into a histogram!
+                 ''')
+
+df_scatter = df[['xp','natural_ac','challenge_rating','strength','dexterity','intelligence','wisdom','constitution','charisma']]
+df_scatter.columns= df_scatter.columns.str.title().str.strip().str.replace('_',' ')
+
+col1,col2 = st.columns(2)
+
+sx = col1.selectbox('Choose your X Coordinate', placeholder="Challenge Rating", options=sorted(df_scatter.columns))
+
+sy = col2.selectbox('Choose your Y Coordinate', placeholder="Xp", options=sorted(df_scatter.columns))
+
+if histo:
+    try:
+        st.plotly_chart(px.scatter(df_histo,sx,sy))
+    except BaseException:
+        st.error(f'''
+                 {sx.title()} could not be plotted into a histogram!
                  ''')
